b66281285a3a92bffc9c9685269bd237a174eda6
Have Samuel <samiehave9@gmail.com>
2023-06-20 23:31:49 +0300

    Create the Clicking EventListener

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 const diceIcon = document.querySelector('.changer');
 const adviceNum = document.querySelector('.advice-num');
 const paragraph = document.querySelector('.paragraph');
+
+// When the dice icon is clicked,
+// the event fetches data from ADVICE SLIP API and displays the number of the advice and advice.
+diceIcon.addEventListener('click', () => {
+  // console.log('Hello!');
+
+});

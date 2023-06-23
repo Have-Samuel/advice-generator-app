6f39c9ed1cf6f0a2a488ccdbec02183d36eb7978
Have Samuel <samiehave9@gmail.com>
2023-06-23 03:24:05 +0300

    Displaying the Data from the API

--- app.js
+++ app.js
@@ -2,9 +2,28 @@ const diceIcon = document.querySelector('.changer');
 const adviceNum = document.querySelector('.advice-num');
 const paragraph = document.querySelector('.paragraph');
 
+// Display the number of the advice and advice.
+const displayAdvice = (data) => {
+  adviceNum.textContent = data.id;
+  paragraph.textContent = data.advice;
+
+  console.log(data.slip.id);
+};
+
+// Fetch data from ADVICE SLIP API.
+const fetchData = () => {
+  fetch('https://api.adviceslip.com/advice')
+    .then((response) => response.json());
+};
+
 // When the dice icon is clicked,
 // the event fetches data from ADVICE SLIP API and displays the number of the advice and advice.
 diceIcon.addEventListener('click', () => {
-  // console.log('Hello!');
-  
+  console.log('Hello!');
+  fetchData();
+  displayAdvice();
 });
+
+// Call the fetchData function.
+
+// Call the displayAdvice function.

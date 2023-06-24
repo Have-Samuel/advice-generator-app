b385b2b31108961d2ece241d271ea1bc69b6f2fa
Have Samuel <samiehave9@gmail.com>
2023-06-24 21:25:13 +0300

    Styling and making use of Async and awiat

--- app.js
+++ app.js
@@ -2,10 +2,21 @@ const diceIcon = document.querySelector('.changer');
 const adviceNum = document.querySelector('.advice-num');
 const paragraph = document.querySelector('.paragraph');
 
-// Display the number of the advice and advice.
-// Using async and awaits
-async function getData() {
-  const response = await axios.get('https://api.adviceslip.com/advice');
-  console.log(response);
-  console.log('THIS MSG IS AFTER AXIOS.GET!!');
+// Using the Axios library to make a GET request
+async function getAdvice() {
+  // const axios = require('axios');
+  const response = await axios.get('https://api.adviceslip.com/advice')
+    .then((response) => {
+      // handle success
+      console.log(response.data.slip.advice);
+      adviceNum.innerHTML = response.data.slip.id;
+      paragraph.innerHTML = response.data.slip.advice;
+    })
+    .catch((error) => {
+      // handle error
+      console.log(error);
+    });
 }
+
+// Event listener
+diceIcon.addEventListener('click', getAdvice);

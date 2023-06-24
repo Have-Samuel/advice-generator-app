43383839ea280dd2d22fc5630956d00d4e13530e
Have Samuel <samiehave9@gmail.com>
2023-06-24 20:32:15 +0300

    Using Async and Await

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const adviceNum = document.querySelector('.advice-num');
 const paragraph = document.querySelector('.paragraph');
 
 // Display the number of the advice and advice.
-function getData() {
-  const response = axios.get('https://api.adviceslip.com/advice');
+// Using async and awaits
+async function getData() {
+  const response = await axios.get('https://api.adviceslip.com/advice');
+  console.log(response);
   console.log('THIS MSG IS AFTER AXIOS.GET!!');
 }

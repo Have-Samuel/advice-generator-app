f086bd5b88eb09945682fb3a035abcf8f95799ed
Have Samuel <samiehave9@gmail.com>
2023-06-24 20:29:20 +0300

    Using  function

--- app.js
+++ app.js
@@ -3,4 +3,7 @@ const adviceNum = document.querySelector('.advice-num');
 const paragraph = document.querySelector('.paragraph');
 
 // Display the number of the advice and advice.
-const response = axios.get('https://api.adviceslip.com/advice');
+function getData() {
+  const response = axios.get('https://api.adviceslip.com/advice');
+  console.log('THIS MSG IS AFTER AXIOS.GET!!');
+}

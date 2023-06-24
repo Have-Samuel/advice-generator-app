8f3c8abd04629089057b289871194c79c4de6b52
Have Samuel <samiehave9@gmail.com>
2023-06-24 21:47:12 +0300

    Adjusting the styling and Clear showing the Heading

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
+/* eslint-disable no-undef */
 const diceIcon = document.querySelector('.changer');
 const adviceNum = document.querySelector('.advice-num');
 const paragraph = document.querySelector('.paragraph');
 
 // Using the Axios library to make a GET request
-async function getAdvice() {
-  // const axios = require('axios');
-  const response = await axios.get('https://api.adviceslip.com/advice')
+function getAdvice() {
+  axios.get('https://api.adviceslip.com/advice')
     .then((response) => {
       // handle success
       console.log(response.data.slip.advice);
-      adviceNum.innerHTML = response.data.slip.id;
+
+      let Advice;
+      adviceNum.innerHTML = `${Advice}#${response.data.slip.id}`;
       paragraph.innerHTML = response.data.slip.advice;
     })
     .catch((error) => {

aebbc329c8afa8883cf9eda95acc02594b8f5651
Have Samuel <samiehave9@gmail.com>
2023-06-24 20:20:43 +0300

    Add AXIOS to HTML file

--- app.js
+++ app.js
@@ -3,18 +3,4 @@ const adviceNum = document.querySelector('.advice-num');
 const paragraph = document.querySelector('.paragraph');
 
 // Display the number of the advice and advice.
-const displayAdvice = (data) => {
-  
-};
-
-// When the dice icon is clicked,
-// the event fetches data from ADVICE SLIP API and displays the number of the advice and advice.
-diceIcon.addEventListener('click', () => {
-  console.log('Hello!');
-  // fetchData();
-  displayAdvice();
-});
-
-// Call the fetchData function.
-
-// Call the displayAdvice function.
+const response = axios.get('https://api.adviceslip.com/advice');
